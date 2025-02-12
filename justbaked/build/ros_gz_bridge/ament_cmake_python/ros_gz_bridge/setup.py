a0cc21bfb7d66e3406f82bf603a21a22d5cb8e54
Jackson Galloway <gfamily4608@gmail.com>
2025-02-12 04:16:55 +0000

    Restarted to have a better start when building the robot in urdf, rviz and gazebo.

--- justbaked/build/ros_gz_bridge/ament_cmake_python/ros_gz_bridge/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='ros_gz_bridge',
-    version='1.0.8',
-    packages=find_packages(
-        include=('ros_gz_bridge', 'ros_gz_bridge.*')),
-)
--- justbaked/build/ros_gz_sim/ament_cmake_python/ros_gz_sim/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='ros_gz_sim',
-    version='1.0.8',
-    packages=find_packages(
-        include=('ros_gz_sim', 'ros_gz_sim.*')),
-)
