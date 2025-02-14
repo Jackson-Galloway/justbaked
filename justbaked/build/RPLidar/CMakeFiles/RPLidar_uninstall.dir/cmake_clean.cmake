a8ee75a2306b6bd3a96a29bf68f421a292d1b6a9
Jackson Galloway <gfamily4608@gmail.com>
2025-02-14 19:10:28 +0000

    Adding the robot urdf file for physical simulation as well as building the environment and adding the joint state publisher.

---
+++ justbaked/build/RPLidar/CMakeFiles/RPLidar_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RPLidar_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/RPLidar_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
