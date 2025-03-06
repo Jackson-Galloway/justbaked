6c5ba8b5a8c9499847e0ae0ae30bc01bb6686608
jack <lsw155linuxbox@jack.astate.edu>
2025-03-05 18:31:01 -0600

    Adding Turtle Files and RPLidar Files

--- justbaked/build/robdesc/CMakeFiles/robdesc_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/robdesc_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/robdesc_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
